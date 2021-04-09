ffdc3cdb0407e3e9bda14136320da5516b1dca9f
Gary Rivera <a.gary.rivera@gmail.com>
2021-04-09 11:30:37 -0700

    setup components

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,12 @@
 import Card from './Card';
+import axios from 'axios';
+
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
 
 function Deck() {
+
+  
+
   return (
   <div> 
     <Card /> 
