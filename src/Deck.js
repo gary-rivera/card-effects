fca05fb4c49830553a6d8483fb8426114857819f
johnathanraiss <johnathan.raiss@gmail.com>
2021-04-09 19:49:54 -0400

    Finished

--- src/Deck.js
+++ src/Deck.js
@@ -7,6 +7,10 @@ const API_URL = 'https://deckofcardsapi.com/api/deck';
 function Deck() {
   const [deck, setDeck] = useState(null);
   const [drawnCards, setDrawnCards] = useState([]);
+
+  const [isShuffled, setIsShuffled] = useState(false);
+
+
   
   useEffect(function processGame() { 
     async function fetchDeck() {
@@ -38,6 +42,24 @@ function Deck() {
       alert(err);
     }
   }
+
+  useEffect(function shuffleDeck() {
+    async function shuffle(deck) {
+      changeShuffleState()
+      ////update deck state in request to the api with setter.
+      await axios.get(`${API_URL}/${deck.deck_id}/shuffle`)
+      //return to initial states(setDrawn and setShuffled) after shuffling.
+      setIsShuffled(false);
+      //Set to an empty array for display purposes.
+      setDrawnCards([])
+    }
+    if (isShuffled && deck) shuffle(deck);
+  }, [isShuffled, deck]);
+
+  
+  function changeShuffleState() {
+    setIsShuffled(true);
+  }  
   
   function renderButton() {
     if (!deck) return null;
@@ -48,9 +70,19 @@ function Deck() {
     );
   }
 
+  function shuffleButton() {
+    if (!deck) return null;
+    return(
+      <button onClick={changeShuffleState}>
+        Shuffle the Deck.
+      </button>
+    );
+  }
+
   return (
   <div className="board"> 
       { renderButton() }
+      { shuffleButton() }
     <div className="cards">
       {drawnCards.map(card => (
         <Card key={card.id} image={card.image} name={card.name}/>
